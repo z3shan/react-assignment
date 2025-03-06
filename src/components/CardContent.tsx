cb7ee472edc5cf42c810ba62b566f3b6ad3afc15
Zeeshan Mirza <zeeshan.m@intelmatix.ai>
2025-03-06 17:48:24 +0500

    Remove unused code

--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
 import {JSX} from "react";
-
-interface CardContentProps {
-    children: React.ReactNode;
-}
-
 const CardContent: ({children}: { children: any }) => JSX.Element = ({ children }) => {
     return <div className="p-2">{children}</div>;
 };
